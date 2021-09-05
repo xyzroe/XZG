a757a4cd11a8d219e10757b6c748c05e0195f0ab
xyzroe <me@kogan.in.ua>
2021-09-06 00:59:39 +0300

    Increase version after push

--- python/version_increment_post.py
+++ python/version_increment_post.py
@@ -22,8 +22,9 @@ import os
 #  You should have received a copy of the MIT License along with this program.
 #  If not, see <https://opensource.org/licenses/MIT/>.
 #
-if os.path.exists(".version_no_increment"):
-    os.remove(".version_no_increment")
+if os.path.exists("../../.version_no_increment"):
+    os.remove("../../.version_no_increment")
+
 #def remove_guard_file(source, target, env):
 #    """ Remove version increment guard file if present """
 #    if os.path.exists(".version_no_increment"):
--- src/Version.h
+++ src/Version.h
@@ -4,6 +4,6 @@
         #define VERSION "0.3.15"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-09-06 00:51:33.304087"
+        #define BUILD_TIMESTAMP "2021-09-06 00:58:50.804088"
     #endif
     
