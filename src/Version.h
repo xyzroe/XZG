7fd8cc6f3e0df4674d42668664c5b61caa022b7d
xyzroe <me@kogan.in.ua>
2021-09-06 01:12:22 +0300

    Reworked version_increment_post.py

--- src/Version.h
+++ src/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.3.15"
+        #define VERSION "0.3.16"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-09-06 01:07:02.031080"
+        #define BUILD_TIMESTAMP "2021-09-06 01:11:44.736301"
     #endif
     
