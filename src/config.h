f07930982561e155b3ca77898a377076ea7142f5
xyzroe <me@kogan.in.ua>
2021-09-05 15:05:10 +0300

    Add src folder

---
+++ src/config.h
@@ -0,0 +1,56 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <Arduino.h>
+#include <CircularBuffer.h>
+
+#define VERSION "v0.3"
+// hardware config
+#define RESET_ZIGBEE 33//13
+#define FLASH_ZIGBEE 32//14
+#define PRODUCTION 1
+#define FLASH 0
+
+#define RXD2 5//2 //16
+#define TXD2 17//4 //17
+
+// ma structure config
+struct ConfigSettingsStruct {
+  bool enableWiFi;
+  char ssid[50];
+  char password[50];
+  char ipAddressWiFi[18];
+  char ipMaskWiFi[16];
+  char ipGWWiFi[18];
+  bool dhcp;
+  bool connectedEther;
+  char ipAddress[18];
+  char ipMask[16];
+  char ipGW[18];
+  int serialSpeed;
+  int socketPort;
+  int  tcpListenPort;
+  bool disableWeb;
+  bool enableHeartBeat;
+  double refreshLogs;
+};
+
+struct InfosStruct {
+  char device[8];
+  char mac[8];
+  char flash[8];
+};
+
+typedef CircularBuffer<char, 1024> LogConsoleType;
+
+
+#define DEBUG_ON
+
+#ifdef DEBUG_ON
+#define DEBUG_PRINT(x)  Serial.print(x)
+#define DEBUG_PRINTLN(x)  Serial.println(x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#endif
+#endif
