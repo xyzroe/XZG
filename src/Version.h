ae9dd9afad40f63a4f09d1ba635a8d6e7447f0fb
xyzroe <me@kogan.in.ua>
2021-09-06 01:08:21 +0300

    Increase version after push

--- src/Version.h
+++ src/Version.h
@@ -4,6 +4,6 @@
         #define VERSION "0.3.15"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-09-06 01:05:47.195548"
+        #define BUILD_TIMESTAMP "2021-09-06 01:07:02.031080"
     #endif
     
