cd6c9b517be90930f0eebe4e2949d6898008862a
xyzroe <me@kogan.in.ua>
2021-09-13 19:38:02 +0300

    Version number fix

--- src/Version.h
+++ src/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.4.1"
+        #define VERSION "0.4.0"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-09-13 18:56:20.798579"
+        #define BUILD_TIMESTAMP "2021-09-13 19:33:16.180888"
     #endif
     
