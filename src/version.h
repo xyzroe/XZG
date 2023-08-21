be08ca7fe09bc887c841c98a652836a09371d2c7
xyzroe <me@kogan.in.ua>
2023-08-21 16:20:20 +0300

    Almost done build on GitHub

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.1(ZigStar)"
+        #define VERSION "0.1.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP __DATE__
+        #define BUILD_TIMESTAMP "2023-08-21 16:18:22.105517"
     #endif
     
