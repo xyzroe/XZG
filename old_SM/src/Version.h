9412ea125e26b8238a99664fd4fc08f8accee5fc
xyzroe <me@kogan.in.ua>
2024-06-02 18:42:33 +0200

    Add and remove smlight-dev/slzb-06-firmware repository files to include all contributors

--- old_SM/src/Version.h
+++
@@ -1,9 +0,0 @@
-
-    // AUTO GENERATED FILE, DO NOT EDIT
-    #ifndef VERSION
-        #define VERSION "0.9.9"
-    #endif
-    #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP __DATE__
-    #endif
-    
